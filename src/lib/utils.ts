f2ae77605a284a522de7f571955ed6296be1c31a
Hafizh <Hafizh@gmail.com>
2025-08-18 16:07:49 +0700

    private route untuk akses dashboard admin

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,8 +1,27 @@
+import type { LoginResponse } from "@/services/auth/auth_type"
 import { clsx, type ClassValue } from "clsx"
+import secureLocalStorage from "react-secure-storage"
 import { twMerge } from "tailwind-merge"
 
 export const SESSION_KEY = 'SESSION_KEY'
 
+type Session = Pick<LoginResponse, 'email' | 'name' | 'photoUrl' | 'role'>
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function getSession() {
+  const session = secureLocalStorage.getItem(SESSION_KEY) as Session;
+
+  if (!session) {
+    return null;
+  }
+
+  return {
+    email: session?.email,
+    name: session?.name,
+    role: session?.role,
+    photoUrl: session?.photoUrl,
+  }
 }
--- src/router/adminRoutes.tsx
+++ src/router/adminRoutes.tsx
@@ -1,8 +1,9 @@
 
-import type { RouteObject } from "react-router-dom";
+import { redirect, type RouteObject } from "react-router-dom";
 import AdminLoginPage from "../pages/admin/index.tsx";
 import AdminOverview from "@/pages/adminOverview/index.tsx";
 import AdminLayout from "@/components/AdminLayout.tsx";
+import { getSession } from "@/lib/utils.ts";
 
 const adminRoutes: RouteObject[] = [
     {
@@ -12,6 +13,18 @@ const adminRoutes: RouteObject[] = [
     {
         path: '/admin',
         element: <AdminLayout />,
+        // jaga halaman
+        loader: () => {
+            const user = getSession()
+
+            console.log(user);
+
+            if (!user || user?.role !== 'admin') {
+                throw redirect('/admin/login')
+            }
+
+            return user;
+        },
         children: [
             {
                 index: true,
